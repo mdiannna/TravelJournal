fe7235a81923e9778758a7963bb592ad24f81d93
Diana <mdiannna@yahoo.com>
2020-06-10 15:08:24 +0300

    UI testing done, all tests run successfully

--- app/src/androidTest/java/com/example/traveljournal/TestUI.kt
+++ app/src/androidTest/java/com/example/traveljournal/TestUI.kt
@@ -6,8 +6,7 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import org.junit.Before
@@ -15,15 +14,13 @@ import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.mockito.junit.MockitoJUnit.rule
 
-//import androidx.test.rule.ActivityTestRule
 import androidx.test.runner.AndroidJUnit4
 import com.example.traveljournal.views.MainActivity
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
-class ChangeTextBehaviorTest {
+class TestUI {
 
     private lateinit var stringToBetyped: String
 
@@ -45,16 +42,41 @@ class ChangeTextBehaviorTest {
             .check(matches(isDisplayed()))
     }
 
-//
-//    @Test
-//    fun changeText_CreateJournalActivity() {
-//        // Type text and then press the button.
-//        onView(withId(R.id.editJournalName))
-//            .perform(typeText(stringToBetyped), closeSoftKeyboard())
-//        onView(withId(R.id.JournalNext)).perform(click())
-//
-//        // Check that the text was changed.
-//        onView(withId(R.id.textToBeChanged))
-//            .check(matches(withText(stringToBetyped)))
-//    }
+    fun openCreateJournalActivity() {
+        onView(withId(R.id.btnCreateJournal))
+            .perform(click())
+    }
+
+    @Test
+    fun NextBtnIsDisplayed_CreateJournalActivity() {
+        ActivityScenario.launch(MainActivity::class.java)
+        openCreateJournalActivity()
+
+        onView(withId(R.id.JournalNext))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun changeText_CreateJournalActivity() {
+        ActivityScenario.launch(MainActivity::class.java)
+
+        openCreateJournalActivity()
+
+        onView(withId(R.id.editJournalName))
+            .perform(typeText(stringToBetyped))
+
+        onView(withId(R.id.editJournalName))
+            .check(matches(withText(stringToBetyped)))
+    }
+
+
+    @Test
+    fun openCreateJournalPageActivity() {
+        ActivityScenario.launch(MainActivity::class.java)
+
+        openCreateJournalActivity()
+
+        onView(withId(R.id.JournalNext))
+            .perform(click())
+    }
 }
